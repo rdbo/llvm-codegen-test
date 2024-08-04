90ac1011d0bd73fd73f41c4d79f516ba36af9b3d
rdbo <rdbodev@gmail.com>
2024-08-04 08:00:20 -0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "llvm.hpp"
+
+int main()
+{
+	llvm::LLVMContext context;
+	auto builder = llvm::IRBuilder<>(context);
+	auto module = llvm::Module("<module>", context);
+
+	// Add printf declaration
+	auto printf_type = llvm::FunctionType::get(builder.getInt32Ty(), { builder.getPtrTy() }, true);
+	auto printf_func = llvm::Function::Create(printf_type, llvm::Function::ExternalLinkage, "printf", module);
+
+	// Generate main function declaration
+	auto ret_type = builder.getInt32Ty();
+	std::vector<llvm::Type *> param_types = {};
+	auto function_type = llvm::FunctionType::get(ret_type, param_types, false);
+	auto function_name = "main";
+	llvm::Function::Create(function_type, llvm::Function::ExternalLinkage, function_name, module);
+
+	// Create main function body
+	auto function = module.getFunction(function_name);
+	auto entry_block = llvm::BasicBlock::Create(context, "entry", function);
+	builder.SetInsertPoint(entry_block);
+
+	// Call printf
+	auto fmt = builder.CreateGlobalStringPtr("Hello, World!\n");
+	std::vector<llvm::Value *> args = { fmt };
+	builder.CreateCall(printf_func, args);
+
+	// Generate return
+	auto ret_val = llvm::Constant::getNullValue(builder.getInt32Ty());
+	builder.CreateRet(ret_val);
+
+	// Dump module
+	module.dump();
+
+	return 0;
+}
